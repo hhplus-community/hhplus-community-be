dd9d9e4e45e88b336cfdfb4fe200e669b5c3e91b
eunsol-an <dev.eunsol@gmail.com>
2025-06-18 20:33:42 +0900

    git init

---
+++ src/main/java/org/hhplus/hhpluscommnunity/HhplusCommnunityApplication.java
@@ -0,0 +1,13 @@
+package org.hhplus.hhpluscommnunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusCommnunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusCommnunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hhplus/hhpluscommnunity/HhplusCommnunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhplus.hhpluscommnunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCommnunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
